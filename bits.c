4101f7c437778948d932e5efde8c5625a8b4fd1d
Avery Jones <alj020@ucsd.edu>
2016-10-13 16:30:30 -0700

    asdf

--- bits.c
+++ bits.c
@@ -393,7 +393,7 @@ unsigned float_i2f(int x)
   
   //create mantissa
   int mantissa = 0;
-  int yo = 0x80000000 >> (32-leadingOneBit)
+  int yo = 0x80000000 >> (32-leadingOneBit);
   int mantissaMask = ~(0x80000000 >> (31-leadingOneBit));
   mantissa = mantissaMask & 0x0007FFFF;
   printf("mantissa: %08x\n", mantissa);
